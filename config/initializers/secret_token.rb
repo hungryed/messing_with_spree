3c6d19f0771029c51fc689086879f097e4d220b5
Joe Sutton <hungryed6@gmail.com>
2014-02-18 11:40:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MessingWithSpree::Application.config.secret_key_base = 'c190acd1f1473448194db1153569e02e26223020c9077c3fc54a07ecea0225875f3de6f0a588f0de9d381320cf639cc31f1d0cfd67252900374cc335114ca10d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MessingWithSpree::Application.config.session_store :cookie_store, key: '_messing_with_spree_session'
